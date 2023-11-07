5c3f039c026968c5b48d9f9799d19e3bc7c177c3
Jan Maszkiewicz <jan.maszkiewicz@wp.pl>
2023-11-07 16:44:36 +0100

    html-new countdown added, js-functions are organised and closed into an arow functions

--- JS/js.js
+++ JS/js.js
@@ -1,66 +1,60 @@
-console.log("Helo world!");
-
-let themeName = document.querySelector(".theme__name");
-let button = document.querySelector(".button");
-let bodyElement = document.querySelector(".body");
-let header = document.querySelector(".header");
-let navLink = document.querySelectorAll(".navigation__link");
-let sectionLinks = document.querySelectorAll(".section__links");
-let sectionLink = document.querySelectorAll(".section__link");
-let tableCaption = document.querySelector(".table__caption");
-
-
-button.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark");
-  header.classList.toggle("dark");
-  button.classList.toggle("dark");
-  navLink.forEach((navLink) => navLink.classList.toggle("dark"));
-  tableCaption.classList.toggle("dark");
-  sectionLinks.forEach((sectionLinks) =>
-    sectionLinks.classList.toggle("link--dark")
-  );
-  sectionLink.forEach((sectionLink) => sectionLink.classList.toggle("dark"));
-  themeName.innerText = bodyElement.classList.contains("dark")
-    ? "Jasny"
-    : "Ciemny";
-});
-
-let count = document.querySelector(".countdown");
-let howMuch = document.querySelector(".howMuch");
-let day = document.querySelector(".day").innerText;
-
-switch (day) {
-  case "poniedziałek":
-    howMuch.innerText = "6";
-    break;
-  case "wtorek":
-    howMuch.innerText = "5";
-    break;
-  case "środa":
-    howMuch.innerText = "4";
-    break;
-  case "czwartek":
-    howMuch.innerText = "3";
-    break;
-  case "piątek":
-    howMuch.innerText = "2";
-    break;
-  case "sobota":
-    howMuch.innerText = "1";
-    break;
+const welcome = () => {
+  console.log("Helo world!");
 }
+welcome();
+
+const switchToDark = () => {
+  const themeName = document.querySelector(".theme__name");
+  const button = document.querySelector(".button");
+  const bodyElement = document.querySelector(".body");
+  const header = document.querySelector(".header");
+  const navLink = document.querySelectorAll(".navigation__link");
+  const sectionLinks = document.querySelectorAll(".section__links");
+  const sectionLink = document.querySelectorAll(".section__link");
+  const tableCaption = document.querySelector(".table__caption");
 
 
-howMuch.innerText = (day === "poniedziałek")
-  ? "6"
-  : ((day === "wtorek")
-    ? "5"
-    : ((day === "środa")
+  button.addEventListener("click", () => {
+    bodyElement.classList.toggle("dark");
+    header.classList.toggle("dark");
+    button.classList.toggle("dark");
+    navLink.forEach((navLink) => navLink.classList.toggle("dark"));
+    tableCaption.classList.toggle("dark");
+    sectionLinks.forEach((sectionLinks) =>
+      sectionLinks.classList.toggle("link--dark")
+    );
+    sectionLink.forEach((sectionLink) => sectionLink.classList.toggle("dark"));
+    themeName.innerText = bodyElement.classList.contains("dark")
+      ? "Jasny"
+      : "Ciemny";
+  });
+}
+switchToDark();
+
+{
+  const jsday = document.querySelector(".js-day")
+  
+  jsday.addEventListener("input", () => {
+
+    const day = document.querySelector(".day").value;
+    const howMuch = document.querySelector(".howMuch");
+
+    howMuch.innerText = (day === "poniedziałek")
       ? "4"
-      : ((day === "czwartek")
+      : ((day === "wtorek")
         ? "3"
-        : ((day === "piątek")
+        : ((day === "środa")
           ? "2"
-          : "1"
-        ))));
+          : ((day === "czwartek")
+            ? "1"
+            : ((day === "piątek")
+              ? "7"
+              : ((day === "sobota")
+                ? "6"
+                : "5"
+              ))))
+      )
+  });
 
+
+}
